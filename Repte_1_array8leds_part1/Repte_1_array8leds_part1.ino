ed63b421678fb47cd093e7e5ebaceb4a2e44de7f
nuriacserra <nucose@hotmail.es>
2017-01-25 10:51:50 +0100

    repte 1

---
+++ Repte_1_array8leds_part1/Repte_1_array8leds_part1.ino
@@ -0,0 +1,66 @@
+/********************************************************************************************************
+ **
+ **                    Repte 1. Array de 8 Led's     
+ **                                Part 1
+ **                            
+ **   Nom: Núria Cordero Serra               Date: 25/01/2017
+ *********************************************************************************************************/
+//****************************Include**********************************
+
+//****************************Variables********************************
+
+const int Led0 = 3;
+const int Led1 = 4;
+const int Led2 = 5;
+const int Led3 = 6;
+const int Led4 = 7;
+const int Led5 = 9;
+const int Led6 = 10;
+const int Led7 = 11;
+const int Led8 = 12;
+
+//****************************Setup************************************
+
+void setup()
+{
+  pinMode(Led0,OUTPUT);
+  pinMode(Led1,OUTPUT);
+  pinMode(Led2,OUTPUT);
+  pinMode(Led3,OUTPUT);
+  pinMode(Led4,OUTPUT);
+  pinMode(Led5,OUTPUT);
+  pinMode(Led6,OUTPUT);
+  pinMode(Led7,OUTPUT);
+  pinMode(Led8,OUTPUT);
+}
+//***************************Loop**************************************
+
+void loop ()
+{
+  digitalWrite(Led0, HIGH);
+  digitalWrite(Led1, HIGH);
+  digitalWrite(Led2, HIGH);
+  digitalWrite(Led3, HIGH);
+  digitalWrite(Led4, HIGH);
+  digitalWrite(Led5, HIGH);
+  digitalWrite(Led6, HIGH);
+  digitalWrite(Led7, HIGH);
+  digitalWrite(Led8, HIGH);
+
+  delay(500);
+
+
+  digitalWrite(Led0, LOW );
+  digitalWrite(Led1, LOW );
+  digitalWrite(Led2, LOW );
+  digitalWrite(Led3, LOW );
+  digitalWrite(Led4, LOW );
+  digitalWrite(Led5, LOW );
+  digitalWrite(Led6, LOW );
+  digitalWrite(Led7, LOW );
+  digitalWrite(Led8, LOW );
+
+   delay (500);
+  
+}
+//***************************Funcions**********************************
