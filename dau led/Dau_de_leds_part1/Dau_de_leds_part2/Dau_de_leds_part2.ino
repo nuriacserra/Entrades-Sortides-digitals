d3c7dba6fc93113f4ef27983875f6665a30f08a9
nuriacserra <nucose@hotmail.es>
2017-02-15 09:17:58 +0100

    dau part 2

---
+++ dau led/Dau_de_leds_part1/Dau_de_leds_part2/Dau_de_leds_part2.ino
@@ -0,0 +1,75 @@
+/********************************************************************************************************
+ **
+ **                           Repte 4. Dau de LEDs 
+ **                               PArt 1 
+ **                            
+ **   Nom: Núria Cordero Serra               Date: 15/02/2017
+ *********************************************************************************************************/
+//****************************Include**********************************
+
+
+//****************************Variables********************************
+const int leds4 = 4;          
+const int leds3 = 5;          
+const int leds2 = 6;         
+const int led1  = 7;          
+const int boto = 9;
+boolean botoEstat =LOW;
+
+int num = 1;
+
+//****************************Setup************************************
+void setup()
+{
+  pinMode(leds4, OUTPUT);     
+  pinMode(leds3, OUTPUT);     
+  pinMode(leds2, OUTPUT);     
+  pinMode(led1, OUTPUT);     
+  pinMode(boto,INPUT);
+  
+  digitalWrite(leds4, LOW);   
+  digitalWrite(leds3, LOW);  
+  digitalWrite(leds2, LOW);   
+  digitalWrite(led1, LOW);   
+  
+}
+//***************************Loop**************************************
+void loop()
+{
+  botoEstat = digitalRead(boto);
+  if (botoEstat == HIGH)
+  { 
+    num = random(1,7);
+    digitalWrite(leds2,LOW);
+    digitalWrite(leds3,LOW);
+    digitalWrite(leds4,LOW);
+    digitalWrite(led1,LOW);
+  }
+  switch(num)
+  {
+    case 1:
+      digitalWrite(led1, HIGH);    
+    case 2:      
+      digitalWrite(leds2, HIGH);   
+      break;
+    case 3:  
+      digitalWrite(led1, HIGH);    
+      digitalWrite(leds3, HIGH);  
+      break;
+    case 4:   
+      digitalWrite(leds2, HIGH);   
+      digitalWrite(leds3, HIGH); 
+      break;
+    case 5:
+      digitalWrite(leds2, HIGH);   
+      digitalWrite(leds3, HIGH);
+      digitalWrite(led1, HIGH);    
+      break;
+    case 6:      
+      digitalWrite(leds2, HIGH);
+      digitalWrite(leds3, HIGH);
+      digitalWrite(leds4, HIGH);
+      break;  
+ }
+}
+//***************************Funcions**********************************
